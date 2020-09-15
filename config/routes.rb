c58fbb7e84a9989608c650dfcac3fe4f453d80fd
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-09-15 09:31:13 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists, only: [:index, :show, :create, :new]
+  resources :songs, only: [:index, :show, :create, :new]
+  resources :genres, only: [:index, :show, :create, :new]
+
 end
